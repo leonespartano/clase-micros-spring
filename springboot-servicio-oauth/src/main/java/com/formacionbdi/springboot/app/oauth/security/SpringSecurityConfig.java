2896bd1485b9fa933673eb378bee4ba32529fce4
Juan Ramon <leonespartano@gmail.com>
2022-01-26 20:00:44 -0600

    Se coloco un primer filtro en al autorización

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService usuarioService;
 	
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
+	
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
 		logger.info("Se creo el Bean de passwordEncoder");
@@ -28,7 +32,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder())
+		.and().authenticationEventPublisher(eventPublisher);
 		super.configure(auth);
 	}
 
